74034b0ea47962c04fed1fca6663d675d45315ed
unknown <benjamin-10a@windowslive.com>
2024-12-23 23:07:39 +0100

    main args update

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import argparse
+import algorithms.WCA
 import visualizer
 import benchmark
+import algorithms
 
 def parse_args():
     parser = argparse.ArgumentParser(description="A script to visualize benchmark functions.")
+
+    # Add the --run flag to execute the algorithm
+    parser.add_argument(
+        "--run", "-run",
+        action="store_true",
+        help="Run the specified algorithm on the specified function."
+    )
 
     # Add the --visualize (-vis) flag
     parser.add_argument(
@@ -21,10 +30,52 @@ def parse_args():
         help="Name of the function to visualize."
     )
     
+    # Add the --algorithm (-alg) argument
+    parser.add_argument(
+        "--algorithm", "-alg",
+        type=str,
+        choices=["WCA"],  # Add other algorithm names if needed
+        help="Name of the algorithm to run on the specified function."
+    )
+
     return parser.parse_args()
 
+def run_algorithm(function_name, algorithm):
+    # Dynamically get the function from benchmark module
+    func = getattr(benchmark, function_name, None)
+    if not func:
+        print(f"Function '{function_name}' is not recognized in the benchmark module.")
+        return
+    
+    if algorithm == "WCA":
+        print(f"Running WCA on {function_name}...")
+        algorithms.WCA.wca(func, -5, 5, nvars=2)  # Adjust parameters as needed for WCA
+    else:
+        print(f"Algorithm '{algorithm}' is not recognized.")
+
 def run(args):
+
+    if args.visualize and args.run:
+        print("Cannot use --visualize and --run together. Please choose one.")
+        return
+
+    if args.run:
+        if not args.algorithm:
+            print("Please specify an algorithm with --algorithm or -alg when using --run.")
+        else:
+            run_algorithm(args.function_name, args.algorithm)
+    
     if args.visualize:
+        # Dynamically get the function from benchmark module
+        func = getattr(benchmark, args.function_name, None)
+
+        if func:
+            print(f"Visualizing {args.function_name}...")
+            visualizer.visualize(func, None)
+        else:
+            print(f"Function '{args.function_name}' is not recognized in the benchmark module.")
+
+    '''if args.visualize:
         # Dynamically get the function from benchmark module
         func = getattr(benchmark, args.function_name, None)
         
@@ -34,7 +85,7 @@ def run(args):
         else:
             print(f"Function '{args.function_name}' is not recognized in the benchmark module.")
     else:
-        print("Visualization flag is not set. Use --visualize or -vis to enable it.")
+        print("Visualization flag is not set. Use --visualize or -vis to enable it.")'''
 
 def main():
     args = parse_args()
